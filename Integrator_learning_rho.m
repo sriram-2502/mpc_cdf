63d4aec6c5b5b98ca977559b567e60b575119dae
Sriram Krishnamoorthy <skrishnamoorthy@clearmotion.com>
2024-06-26 16:34:47 -0400

    added single integrator system with tracking example

--- Integrator_learning_rho.m
+++ Integrator_learning_rho.m
@@ -14,62 +14,60 @@ obsColor = [.7 .7 .7]; % Obstacle color -> Grey
 
 %% Setup Parameters
 % ---------- system setup ---------------
-% states for AUV
+% states for integrator
 x = SX.sym('x');
 y = SX.sym('y');
-theta = SX.sym('theta');
-v = SX.sym('v');
-states = [x; y; theta; v];
+rho = SX.sym('rho');
+states = [x; y;];
 n_states = length(states);
 
-% control Inputs for AUV
-w = SX.sym('w');
-a = SX.sym('a');
-controls = [w;a];
+% control Inputs in density space
+rho_bar1 = SX.sym('rho_bar1');
+rho_bar2 = SX.sym('rho_bar2');
+controls = [rho_bar1;rho_bar2];
 n_controls = length(controls);
 
 %---------- MPC setup ----------------------
 time_total = 20; % time for the total steps, equal to tsim
-N = 100; % for mismatch use N = 100
-dt = 0.02; % use dt = 0.1 for cbf and vanilla obs
+N = 10; % for mismatch use N = 100
+dt = 0.01; % use dt = 0.1 for cbf and vanilla obs
 o = 1;
-Q = 10*diag([10,10,o,o]);
-P_weight = 100*diag([10,10,o,o]);
+Q = 10*diag([10,10]);
 R = 1*diag([1, 1]);
 C_t = 0.1;
 
-xmin = [-inf; -inf; -inf; -inf];
+xmin = [-inf; -inf];
 xmax = -xmin;
 
 umin = [-inf; -inf];
 umax = -umin;
 
-% ----------- Environment setup --------------------
-% initial Conditions on a grid
-x0 = [0;0.01;0;0]; x_ini = x0;
-xf = [10;0;0;0]; % target
+% % ----------- Environment setup --------------------
+% % initial Conditions on a grid
+% x0 = [0;0.01]; x_ini = x0;
+% xf = [10;0]; % target
+% 
+% obs_x = SX.sym('obs_x');
+% obs_y = SX.sym('obs_y');
+% obs_r = SX.sym('obs_r');
+% obs_s = SX.sym('obs_s');
+% obs = [obs_x;obs_y;obs_r;obs_s];
+% 
+% % obstacle list for sphere world
+% num_obs = 1; % Number of obstacles
+% obs_rad = 1;
+% obs_sens = obs_rad + 1;
+% obs1 = [4; 0; obs_rad(1); obs_sens(1)];
+% 
+% %---------- cbf/obstalce constraint setup ---------------
+% rho_circle = density_circle(states,obs);
+% rho_circle = Function('b',{states,obs},{rho_circle}); 
 
-obs_x = SX.sym('obs_x');
-obs_y = SX.sym('obs_y');
-obs_r = SX.sym('obs_r');
-obs_s = SX.sym('obs_s');
-obs = [obs_x;obs_y;obs_r;obs_s];
-
-% obstacle list for sphere world
-num_obs = 1; % Number of obstacles
-obs_rad = 1;
-obs_sens = obs_rad + 1;
-obs1 = [4; 0; obs_rad(1); obs_sens(1)];
-
-%---------- cbf/obstalce constraint setup ---------------
-rho_circle = density_circle(states,obs);
-rho_circle = Function('b',{states,obs},{rho_circle}); 
 
 
-
-%% Dynamics Setup 
+%% Dynamics Setup (for divergence constraint)
 % dynamics without paramter mismatch
-[dx_dt,f,g] = unicycle_dynamics(states, controls);
+[dx_dt,f,g] = integrator_dynamics(states, controls);
 f_discrete = dt*f + states;
 g_discrete = dt*g;
 
@@ -86,19 +84,10 @@ div_g_discrete1 = sum(diag(jacob_G_discrete1));
 g_discrete2 = g_discrete(:,2);
 jacob_g_discrete2 = jacobian(g_discrete2, states');
 div_g_discrete2 = sum(diag(jacob_g_discrete2));
-% 
-% g_discrete3 = g_discrete(:,3);
-% jacob_g_discrete3 = jacobian(g_discrete3, states');
-% div_g_discrete3 = sum(diag(jacob_g_discrete3));
-% 
-% g_discrete4 = g_discrete(:,4);
-% jacob_g_discrete4 = jacobian(g_discrete4, states');
-% div_g_discrete4 = sum(diag(jacob_g_discrete4));
 
 % calculate the total divergence of g_discrete
 div_g_discrete = div_g_discrete1+div_g_discrete2;%+div_g_discrete3+div_g_discrete4;
 div_g_discrete = Function('div_G_discrete1',{states},{div_g_discrete});
-
 
 % define matlab functions for F=f+gu, f, g
 F = Function('F',{states,controls},{dx_dt}); 
@@ -110,14 +99,18 @@ g = Function('g',{states},{g});
 % A vector that represents the states over the optimization problem.
 X = SX.sym('X',n_states,(N+1));
 
-% Decision variables for control
-U = SX.sym('U',n_controls,N); 
+% Decision variables for density function values
+rho = SX.sym('rho',1,(N+1));
+
+% Decision variables for control (rho bar)
+rho_bar = SX.sym('U',n_controls, N); 
 
 % Decision variables for slack
 C = SX.sym('C',N); 
 
 % parameters (which include at the initial state of the robot and the reference state)
-P = SX.sym('P',n_states + n_states);
+% P = SX.sym('P',n_states + n_states); % for point stabliziation
+P = SX.sym('P',n_states + N*n_states); % for ref tracking
 
 obj = 0; % Objective function
 constraints = [];  % constraints vector
@@ -130,7 +123,9 @@ constraints = [constraints;st-P(1:n_states)]; % initial condition constraints
 for k = 1:N
     st = X(:,k);
     con = U(:,k);
-    obj = obj+(st-P(n_states+1:2*n_states))'*Q*(st-P(n_states+1:2*n_states)) + con'*R*con; % calculate obj
+%     obj = obj+(st-P(n_states+1:2*n_states))'*Q*(st-P(n_states+1:2*n_states)) + con'*R*con; % calculate obj
+    obj = obj+(st-P(n_states*k+1:n_states*k+n_states))'*Q*(st-P(n_states*k+1:n_states*k+n_states)) + con'*R*con;
+    
     st_next = X(:,k+1);
     f_value = F(st,con);
     st_next_euler = st+ (dt*f_value);
@@ -140,9 +135,6 @@ end
 % Add Terminal Cost
 k = N+1;
 st = X(:,k);
-obj = obj+(st-P(n_states+1:2*n_states))'*P_weight*(st-P(n_states+1:2*n_states)); % calculate obj
-
-
 % density constraint for obstacles
 for obs_num = 1:num_obs
     for k = 1:N
@@ -190,16 +182,11 @@ args.lbx(1:n_states:n_states*(N+1),1) = xmin(1); %state x lower bound
 args.ubx(1:n_states:n_states*(N+1),1) = xmax(1); %state x upper bound
 args.lbx(2:n_states:n_states*(N+1),1) = xmin(2); %state y lower bound
 args.ubx(2:n_states:n_states*(N+1),1) = xmax(2); %state y upper bound
-args.lbx(3:n_states:n_states*(N+1),1) = xmin(3); %state theta lower bound
-args.ubx(3:n_states:n_states*(N+1),1) = xmax(3); %state theta upper bound
-args.lbx(4:n_states:n_states*(N+1),1) = xmin(4); %state velocity lower bound
-args.ubx(4:n_states:n_states*(N+1),1) = xmax(4); %state velocity upper bound
 
-
-args.lbx(n_states*(N+1)+1:n_controls:n_states*(N+1)+n_controls*N,1) = umin(1); %v lower bound
-args.ubx(n_states*(N+1)+1:n_controls:n_states*(N+1)+n_controls*N,1) = umax(1); %v upper bound
-args.lbx(n_states*(N+1)+2:n_controls:n_states*(N+1)+n_controls*N,1) = umin(2); %w lower bound
-args.ubx(n_states*(N+1)+2:n_controls:n_states*(N+1)+n_controls*N,1) = umax(2); %w upper bound
+args.lbx(n_states*(N+1)+1:n_controls:n_states*(N+1)+n_controls*N,1) = umin(1); %u1 lower bound
+args.ubx(n_states*(N+1)+1:n_controls:n_states*(N+1)+n_controls*N,1) = umax(1); %u1 upper bound
+args.lbx(n_states*(N+1)+2:n_controls:n_states*(N+1)+n_controls*N,1) = umin(2); %u2 lower bound
+args.ubx(n_states*(N+1)+2:n_controls:n_states*(N+1)+n_controls*N,1) = umax(2); %u2 upper bound
 
 args.lbx(n_states*(N+1)+n_controls*N+1:1:n_states*(N+1)+n_controls*N+N,1) = 0; %C lower bound
 args.ubx(n_states*(N+1)+n_controls*N+1:1:n_states*(N+1)+n_controls*N+N,1) = inf; %C upper bound
@@ -224,14 +211,27 @@ w_bar = waitbar(0,'1','Name','Simulating MPC-CDF...',...
 
 while(norm((x0-xf),2) > 1e-2 && mpciter < time_total / dt)
     max_iter = time_total/dt;
+    current_time = mpciter*dt;
     waitbar(mpciter/max_iter,w_bar,sprintf(string(mpciter)+'/'+string(max_iter)))
     
-    args.p   = [x0;xf]; % set the values of the parameters vector
-
+    % set the values of the parameters vector
+%     args.p   = [x0;xf]; 
+    args.p(1:n_states) = x0;
+    for k=1:N
+        t_predict = current_time + (k-1)*dt;
+        x_ref = 0.5*t_predict;
+        y_ref = 1;
+        if(x_ref >=12)
+            xref  = 12;
+            y_ref = 12;
+        end
+        args.p(n_states*k+1:n_states*k+n_states) = [x_ref, y_ref];
+    end    
+    
     % initial value of the optimization variables
     args.x0  = [reshape(X0',n_states*(N+1),1);reshape(u0',n_controls*N,1);reshape(C_0',N,1)];
     sol = solver('x0', args.x0, 'lbx', args.lbx, 'ubx', args.ubx,...
-        'lbg', args.lbg, 'ubg', args.ubg,'p',args.p);
+        'lbg', args.lbg, 'ubg', args.ubg, 'p', args.p);
     u = reshape(full(sol.x(n_states*(N+1)+1:end-N))',n_controls,N)'; % get controls only from the solution
     xx1(:,1:n_states,mpciter+1)= reshape(full(sol.x(1:n_states*(N+1)))',n_states,N+1)'; % get solution TRAJECTORY
     u_cl= [u_cl ; u(1,:)];
@@ -241,13 +241,10 @@ while(norm((x0-xf),2) > 1e-2 && mpciter < time_total / dt)
     C_log = [C_log; C_0];
 
     % Apply the control and shift the solution
-
     [t0, x0, u0] = shift(dt, t0, x0, u,F);
-
 
     xlog(:,mpciter+1) = x0;
     X0 = reshape(full(sol.x(1:n_states*(N+1)))',n_states,N+1)'; % get solution TRAJECTORY
-
 
     % Shift trajectory to initialize the next step
     X0 = [X0(2:end,:);X0(end,:)];
@@ -260,10 +257,16 @@ delete(F);
 %% ---------------- plot 2D trajectory ----------------------  
 
 figure(1)
-
 % For legend as rectangular object can't be defined as a legend
     dummy_marker = plot(NaN,NaN, 'o','MarkerSize', 10, 'MarkerEdgeColor',...
             'black', 'MarkerFaceColor',obsColor, 'LineWidth', 1.5); 
+        
+% plot obstacles
+xc = obs1(1); yc = obs1(2); Rc = obs1(3);
+angles = (0:100-1)*(2*pi/100);
+points = [xc;yc] + [Rc*cos(angles);Rc*sin(angles)];
+P = polyshape(points(1,:), points(2,:));
+plot(P, 'FaceColor', obsColor, 'LineWidth', 2, 'FaceAlpha', 1.0); hold on;
 
 % plot x-y-z trajecotry
 traj = plot(xlog(1,:), xlog(2,:),'LineWidth', 2,'Color',red);
@@ -275,12 +278,6 @@ hold on
 % plot(x_ini(1), x_ini(2), 'o', 'MarkerSize',10, 'MarkerFaceColor','black','MarkerEdgeColor','black'); hold on;
 % plot(xf(1), xf(2), 'o', 'MarkerSize',10, 'MarkerFaceColor',green,'MarkerEdgeColor',green); hold on;
 
-% plot obstacles
-xc = obs1(1); yc = obs1(2); Rc = obs1(3);
-angles = (0:100-1)*(2*pi/100);
-points = [xc;yc] + [Rc*cos(angles);Rc*sin(angles)];
-P = polyshape(points(1,:), points(2,:));
-plot(P, 'FaceColor', obsColor, 'LineWidth', 2, 'FaceAlpha', 1.0); hold on;
 
 %setup plots
 axes1 = gca;
