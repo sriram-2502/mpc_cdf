16145f9d5b631b31a96082ae4728dc40b19704d5
sriramk <sriramk@clemson.edu>
2025-03-20 10:27:11 -0400

    Revert "added minor bug fixes"
    This reverts commit 94f7edc5a9032b09f8530facd1cd7634b4020de0.

--- density_functions/density_circle.m
+++ density_functions/density_circle.m
@@ -1,4 +1,4 @@
-function rho = density_circle(states,obs)
+function rho = dcnsity_circle(states,obs)
 % Inputs 
 % states    : [x; y; xdot; ydot] -> casadi variables;
 % obs       : [obs_x; obs_y; obs_r; obs_s] -> casadi variables
@@ -19,8 +19,7 @@ function rho = density_circle(states,obs)
 
     f_bar1 = if_else(temp1 > 0, exp(-1/temp1), 0)/(if_else(temp1 > 0, exp(-1/temp1), 0) + if_else(1-temp1 > 0, exp(-1/(1-temp1)), 0));
     Phi = f_bar1;
-%     V = [states(1) states(2) states(3) states(4)] *P_lyap*([states(1); states(2); states(3); states(4)]);
-    V = states' * P_lyap * states;
+    V = [states(1) states(2) states(3) states(4)] *P_lyap*([states(1); states(2); states(3); states(4)]);
     rho = Phi/(V.^alpha);
 
 end
